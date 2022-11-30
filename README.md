d7dce1753250ad338b8f785c7dfa8adc8250a780
Edmund Quainoo <edmundquainoo3018@gmail.com>
2022-11-30 08:20:49 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Flutter UI - Coffee App
+
+English-----(README_CN.md)
+
+### Introduction
+
+The coffee application is divided into 3 pages, namely the splash screen page, the main page and the login page.
+
+
+
+
+### Coffee App Design UI
+
+![00](00.png)
+![000](000.png)
+
+### Coffee App Final UI
+
+<div align=center> <img src = '01.png' width = '300' >  <img src = '02.png' width = '300' > <img src = '03.png' width = '300' > </div>
+
+<div align=center> <img src = '001.png' width = '300' >  <img src = '002.png' width = '300' > <img src = '003.png' width = '300' > </div>
+
+
---
+++ lib/screen/login/widget/login_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_coffee/constant.dart';
+import 'package:flutter_coffee/screen/main/main_screen.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: FlatButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(36),
+        ),
+        color: mPrimaryColor,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return MainScreen();
+              },
+            ),
+          );
+        },
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          alignment: Alignment.center,
+          child: Text(
+            'Login',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  
+  FlatButton({RoundedRectangleBorder shape, Color color, Null Function() onPressed, Container child}) {}
+}
